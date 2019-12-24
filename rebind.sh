a610c969ae407e6befb4c9f1cafef609dbc9885e
Pitter8 <pitter18278@gmail.com>
2019-12-23 21:12:15 -0500

    Update rebind.sh

--- rebind.sh
+++ rebind.sh
@@ -1,6 +1,5 @@
-BIND_PID1="1022 145f"
-BIND_BDF1="0000:27:00.3"
-
+BIND_PID1="1022 7914"
+BIND_BDF1="0000:10:00.0"
 sudo modprobe vfio-pci
 echo "$BIND_PID1" > /sys/bus/pci/drivers/vfio-pci/new_id
 echo "$BIND_BDF1" > /sys/bus/pci/devices/$BIND_BDF1/driver/unbind
